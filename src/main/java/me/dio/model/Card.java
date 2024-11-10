fb0c5914bb8af881c48459013f14f5efd49f103c
rhafaelcm <rhafaelcm@msn.com>
2024-11-10 10:27:21 -0400

    Hotfix "precision" e "scale"

--- src/main/java/me/dio/model/Card.java
+++ src/main/java/me/dio/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(nullable = false, unique = true)
     private String number;
 
-    @Column(name = "available_limit", nullable = false, scale = 13, precision = 2)
+    @Column(name = "available_limit", nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
